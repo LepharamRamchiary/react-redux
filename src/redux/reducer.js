b4264d4d3723e01e40ec373e579155f02bae10ea
Lepharam Ramchiary <lepharamchiary@gamil.com>
2022-06-02 18:57:21 +0530

    create a small cocktail website

---
+++ src/redux/reducer.js
@@ -0,0 +1,79 @@
+import * as types from "./actionTypes";
+
+
+const initialState = {
+    cocktails: [],
+    cocktail: [],
+    loading: false,
+    error: null,
+};
+
+const cockTailReducer =function(state = initialState, action) {
+    switch (action.type) {
+        case types.FETCH_COCKTAIL_START:
+            return {
+                ...state,
+                loading: true
+        };
+        case types.FETCH_COCKTAIL_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                cocktails: action.payload,
+        };
+        case types.FETCH_COCKTAIL_FAIL:
+            return {
+                ...state,
+                loading: false,
+                error: action.payload,
+        };
+
+
+
+        case types.GET_SINGLE_COCKTAIL_START:
+            return {
+                ...state,
+                loading: true
+        };
+        case types.GET_SINGLE_COCKTAIL_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                cocktail: action.payload,
+        };
+        case types.GET_SINGLE_COCKTAIL_FAIL:
+            return {
+                ...state,
+                loading: false,
+                error: action.payload,
+        };
+        
+        default:
+            return state;
+
+
+
+       
+
+            case types.SEARCH_COCKTAIL_START:
+                return {
+                    ...state,
+                    loading: true
+            };
+            case types.SEARCH_COCKTAIL_SUCCESS:
+                return {
+                    ...state,
+                    loading: false,
+                    cocktails: action.payload,
+            };
+            case types.SEARCH_COCKTAIL_FAIL:
+                return {
+                    ...state,
+                    loading: false,
+                    error: action.payload,
+            };
+    };
+
+};
+
+export default cockTailReducer;
